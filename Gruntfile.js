5cb133ec1a38661f2cbf62a6f31a2dd19e8c2cf9
Sebastien Moran <smoran@sierrawireless.com>
2020-05-11 11:06:51 +0200

    Prettier cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,14 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-release');
 
-    grunt.loadNpmTasks("grunt-release");
-
-    grunt.initConfig({
-        release: {
-            options: {
-                github: {
-                    repo: "AirVantage/hubot-jira-issue-fetcher",
-                    accessTokenVar: "GITHUB_ACCESS_TOKEN"
-                }
-            }
-        }
-    });
+  grunt.initConfig({
+    release: {
+      options: {
+        github: {
+          repo: 'AirVantage/hubot-jira-issue-fetcher',
+          accessTokenVar: 'GITHUB_ACCESS_TOKEN',
+        },
+      },
+    },
+  });
 };
--- test/messageParser.js
+++ test/messageParser.js
@@ -2,37 +2,36 @@ import test from 'ava';
 import messageParser from '../lib/messageParser';
 
 const configuration = {
-    jiraHost: 'issues.sierrawireless.com',
-    projectsKeys: 'PLTFRS,PLTBUGS,ALT1250'.split(',')
+  jiraHost: 'issues.sierrawireless.com',
+  projectsKeys: 'PLTFRS,PLTBUGS,ALT1250'.split(','),
 };
 
 const parser = messageParser(configuration);
 
-test('parse simple key', t => {
-    const message = parser.extractIssueKeys({
-        text: 'Working on pltfrs-333. And ALT1250-362, PLTBUGS-3333! '
-    });
-    t.is(message.issueKeys.length, 3);
+test('parse simple key', (t) => {
+  const message = parser.extractIssueKeys({
+    text: 'Working on pltfrs-333. And ALT1250-362, PLTBUGS-3333! ',
+  });
+  t.is(message.issueKeys.length, 3);
 });
 
-
-test('parse URLs', t => {
-    const message = parser.extractIssueKeys({
-        text: 'Working on https://issues.sierrawireless.com/browse/PLTBUGS-3333! '
-    });
-    t.is(message.issueKeys.length, 1);
+test('parse URLs', (t) => {
+  const message = parser.extractIssueKeys({
+    text: 'Working on https://issues.sierrawireless.com/browse/PLTBUGS-3333! ',
+  });
+  t.is(message.issueKeys.length, 1);
 });
 
-test('only returns known issues', t => {
-    const message = parser.extractIssueKeys({
-        text: 'Working on PLTFRS-3456 and TOTO-25'
-    });
-    t.is(message.issueKeys.length, 1);
+test('only returns known issues', (t) => {
+  const message = parser.extractIssueKeys({
+    text: 'Working on PLTFRS-3456 and TOTO-25',
+  });
+  t.is(message.issueKeys.length, 1);
 });
 
-test('returns empty array when no issue found', t => {
-    const message = parser.extractIssueKeys({
-        text: 'Working TOTO-25'
-    });
-    t.is(message.issueKeys.length, 0);
+test('returns empty array when no issue found', (t) => {
+  const message = parser.extractIssueKeys({
+    text: 'Working TOTO-25',
+  });
+  t.is(message.issueKeys.length, 0);
 });
